4d69197f470063de0f3820471c972f1157f08dc3
jun097kim <jun097kim@gmail.com>
2018-07-06 14:43:42 +0900

    pp. 59~73

---
+++ brain_stl/02/ex02-06.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/////// 서버 코드 ////////////
+void PrintHello()
+{
+    cout << "Hello!" << endl;
+}
+
+/////// 클라이언트 코드 ////////
+int main()
+{
+    PrintHello();
+
+    return 0;
+}
