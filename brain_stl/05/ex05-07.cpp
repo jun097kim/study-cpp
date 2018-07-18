809b92953bac0faba8cc1a1cc034f27fc59362f4
jun097kim <jun097kim@gmail.com>
2018-07-18 13:15:46 +0900

    pp. 131~161

---
+++ brain_stl/05/ex05-07.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+
+    v.push_back(50);
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+
+    sort(v.begin(), v.end(), less<int>()); // 오름차순 정렬
+    for (vector<int>::iterator iter = v.begin(); iter != v.end(); ++iter)
+        cout << *iter << endl;
+    cout << endl;
+
+    sort(v.begin(), v.end(), greater<int>()); // 내림차순 정렬
+    for (vector<int>::iterator iter = v.begin(); iter != v.end(); ++iter)
+        cout << *iter << endl;
+    cout << endl;
+
+    return 0;
+}
